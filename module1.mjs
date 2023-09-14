a916e479c3716e90f8480be8226c2daa30de730b
ijaz ahmad <ijazbangashi.b@gmail.com>
2023-09-14 13:16:06 +0500

    initial commit

---
+++ module1.mjs
@@ -0,0 +1,5 @@
+import khan, {b, c, d} from './module2.mjs'
+
+console.log(khan);
+console.log(c);
+console.log(d);
---
+++ module2.mjs
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+const a = "ijaz";
+const b = "fahad";
+const c = "saad";
+const d = "asad";
+
+export  default a;
+export {b};
+export {c};
+export {d};
