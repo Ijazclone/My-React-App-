2f9340306b0dfab4175af4b0e463f2d5624fd62e
ijaz ahmad <ijazbangashi.b@gmail.com>
2023-09-19 22:28:08 +0500

    MA-3 Added a component /alert.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 
 import Navbar from './components/Navbar';
-// eslint-disable-next-line no-unused-vars
+import Alert from './components/alert';
 import TextForm from './components/textForm';
 import About from './components/About';
 import React, { useState } from 'react';
@@ -10,28 +10,39 @@ import React, { useState } from 'react';
 function App() {
   const [mode, setMode] = useState('light');
   const [btnText, setBtnText] = useState('Dark Mode');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+    setAlert({
+          msg: message,
+          type: type
+    })
+  }
       
   const toggleMode = ()=>{
       if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       setBtnText('Light Mode');
-      }else{
+      setAlert("Dark mode has been enabled", "success");
+      }
+      else{
         setMode('light');
         document.body.style.backgroundColor = 'white';
         setBtnText('Dark Mode');
+        setAlert("Light mode has been enabled", "success");
         }
       }
 
   return (
     <>
     
- <Navbar title="My App" homeText="Home" mode={mode} btnText={btnText} toggleMode={toggleMode} aboutText="About Us"/>
-
-<div className="container my-2">
-<TextForm heading="Enter the text khan" mode={mode} />
-<About/> 
-</div>
+  <Navbar title="My App" homeText="Home" mode={mode} btnText={btnText} toggleMode={toggleMode} aboutText="About Us"/>
+  <Alert alert={alert}/>
+  <div className="container my-2">
+  <TextForm heading="Enter the text khan" mode={mode} />
+  <About/> 
+  </div>
  
 
     </>
