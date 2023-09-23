5db733aad0b3b6d77fb574e76a036d5c3329caab
ijazclone <ijazbangashi.b45@gmail.com>
2023-09-23 12:13:59 +0500

    MA-5 just for test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 
 import Navbar from './components/Navbar';
@@ -7,60 +6,80 @@ import TextForm from './components/textForm';
 import About from './components/About';
 import React, { useState } from 'react';
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
 } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light');
-  const [btnText, setBtnText] = useState('Dark Mode');
-  const [alert, setAlert] = useState(null);
+    const [mode, setMode] = useState('light');
+    const [btnText, setBtnText] = useState('Dark Mode');
+    const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type)=>{
-    setAlert({
-          msg: message,
-          type: type
-    })
-    setTimeout(() => {
-      setAlert(null);
-    }, 1500);
-  }
-      
-  const toggleMode = ()=>{
-      if (mode === 'light') {
-      setMode('dark');
-      document.body.style.backgroundColor = '#042743';
-      setBtnText('Light Mode');
-      showAlert("Dark mode has been enabled", "success");
-      }
-      else{
-        setMode('light');
-        document.body.style.backgroundColor = 'white';
-        setBtnText('Dark Mode');
-        showAlert("Light mode has been enabled", "success");
+    const showAlert = (message, type) => {
+        setAlert({
+            msg: message,
+            type: type
+        })
+        setTimeout(() => {
+            setAlert(null);
+        }, 1500);
+    }
+
+    const toggleMode = () => {
+        if (mode === 'light') {
+            setMode('dark');
+            document.body.style.backgroundColor = '#042743';
+            setBtnText('Light Mode');
+            showAlert("Dark mode has been enabled", "success");
+        } else {
+            setMode('light');
+            document.body.style.backgroundColor = 'white';
+            setBtnText('Dark Mode');
+            showAlert("Light mode has been enabled", "success");
         }
-      }
+    }
 
-  return (
-    <>
-  <Router>
-    <Navbar title="My App" homeText="Home" mode={mode} btnText={btnText} toggleMode={toggleMode} aboutText="About Us"/>
-    <Alert alert={alert}/>
-    <div className="container my-2">
-    
-      <Routes>
-              <Route path="/About" element={<About />}/>        
-              
-              <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter the text khan" mode={mode} />}/>
-      </Routes>
-    
-    </div>
-  </Router>
+    return ( <
+        >
+        <
+        Router >
+        <
+        Navbar title = "ijaz 45"
+        homeText = "Home"
+        mode = { mode }
+        btnText = { btnText }
+        toggleMode = { toggleMode }
+        aboutText = "About Us" / >
+        <
+        Alert alert = { alert }
+        /> <
+        div className = "container my-2" >
 
-    </>
-  );
-}
+        <
+        Routes >
+        <
+        Route path = "/About"
+        element = { < About / > }
+        />        
 
-export default App;
+        <
+        Route path = "/"
+        element = { < TextForm showAlert = { showAlert }
+            heading = "Enter the text khan"
+            mode = { mode }
+            />}/ >
+            <
+            /Routes>
+
+            <
+            /div> <
+            /Router>
+
+            <
+            />
+        );
+    }
+
+    export default App;
